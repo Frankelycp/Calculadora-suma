d7f8808b5f9360c537ab056e474640081ae7de17
Usuario <Usuario@DESKTOP-7IM02CJ>
2019-05-14 20:22:06 -0300

    Primer Formulario

--- PrimerFormulario/Form1.Designer.cs
+++ PrimerFormulario/Form1.Designer.cs
@@ -40,18 +40,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(34, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Numero 1:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(37, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Numero 2:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
@@ -74,7 +74,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(115, 49);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
