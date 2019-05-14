ed199936f0afd801615741c9ed9c45261362958f
Usuario <Usuario@DESKTOP-7IM02CJ>
2019-05-14 20:19:48 -0300

    Primer Formulario

--- PrimerFormulario/Form1.cs
+++ PrimerFormulario/Form1.cs
@@ -16,5 +16,10 @@ namespace PrimerFormulario
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
