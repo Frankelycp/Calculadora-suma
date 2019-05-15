60b98963497539e687844074d116cc5823c1e765
Usuario <Usuario@DESKTOP-7IM02CJ>
2019-05-14 21:48:18 -0300

    Comentarios

--- PrimerFormulario/Form1.cs
+++ PrimerFormulario/Form1.cs
@@ -26,14 +26,19 @@ namespace PrimerFormulario
 
 
         private void Calcular_Click(object sender, EventArgs e)
-        {
+        {   //CONVERT.TOINT32 CONVIERTE STRING A ENTEROS...
+
+
             valorCalculado = SumarNumeros(Convert.ToInt32(this.txt1.Text), Convert.ToInt32(this.txt2.Text));
+            //EL SIGNO $ PERMITE JUNTO CON LAS LLAVES MOSTRAR EL VALOR DE UNA VARIABLE
+            // SIN EL SIGNO NO LO PERMITE
             MessageBox.Show($"La suma es : { valorCalculado}");
 
         }
         #region metodos
         private int SumarNumeros(int num1, int num2)
         {
+           
             int valorAuxiliar = 0;
             valorAuxiliar = num1 + num2;
             return valorAuxiliar;
